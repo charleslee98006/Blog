cca06c2a135790f4a3f37d7d0cbad2566ec77bfc
charles <charles.98006@gmail.com>
2015-09-17 13:42:44 -0700

    Creating a blog app using the latest Node, Express, and bootstrap

---
+++ app.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var app = express();
+var hbs = require('hbs');
+
+var blogEngine = require('./blog');
+
+app.set('view engine', 'html');
+app.engine('html', hbs.__express);
+app.use(express.static('public'));
+//app.use(express.bodyParser());
+// //Regular HTTP get
+// app.get(some url, do something);
+ 
+// //Some other page
+// app.get(some other url, do something else);
+ 
+// //I can respond to a form post
+// app.post(some url, do more stuff);
+
+//homepage
+app.get('/', function(request, response){
+  response.render('index',{title:"My Blog", entries:blogEngine.getBlogEntries()});
+});
+
+//about page
+app.get('/about', function(request, response) {
+    response.render('about',{title:"My Blog", entries:blogEngine.getBlogEntries()});
+});
+
+app.get('/article', function(request, response){
+  response.render('article', {title:"About Me"});
+});
+
+app.get('/article/:id', function(req, res) {
+    var entry = blogEngine.getBlogEntry(req.params.id);
+    res.render('article',{title:entry.title, blog:entry});
+});
+
+app.listen(3000);
